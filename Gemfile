b568a755492d2723f15cf6a3772a76505efcce07
Marina Martin <git@marinamartin.com>
2014-01-03 09:43:38 -0500

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,28 @@
+source 'https://rubygems.org'
+ruby '2.0.0'
+#ruby-gemset=folderfinder
+
+# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'rails', '4.0.2'
+
+# Defaults
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
+
+# For logging in
+gem 'devise', '3.0'
+gem 'cancan'
+
+group :doc do
+  # bundle exec rake doc:rails generates the API under doc/api.
+  gem 'sdoc', require: false
+end
+
+group :production do
+  gem 'pg', '0.15.1'
+  gem 'rails_12factor', '0.0.2'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Folderfinder::Application.config.secret_key_base = 'e83b0dd23f088a6c66ddad47d43c5cb33422e242ce04898cceb81e98fd9bf318bf9a63d81c02f2e96f84fdd3a9057480c0ec23ff45d3582fd7fb0ef0af3ecac1'
