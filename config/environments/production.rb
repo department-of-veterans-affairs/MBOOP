2d17a49d6b2750c2d1b2384131d15960e76993c3
Marina Martin <git@marinamartin.com>
2014-02-24 21:47:24 -0500

    Added host to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,9 @@ Folderfinder::Application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  # For Devise
+  config.action_mailer.default_url_options = { :host => 'http://folderfinder.herokuapp.com' }
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both thread web servers
   # and those relying on copy on write to perform better.
