7295d18fe71684834d6be8cdc631567278bb99e4
Marina Martin <git@marinamartin.com>
2014-12-08 23:22:13 -0500

    Added active column to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140323215204) do
+ActiveRecord::Schema.define(version: 20141209042111) do
 
   create_table "folders", force: true do |t|
     t.string   "category"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20140323215204) do
     t.datetime "updated_at"
     t.string   "display_name"
     t.boolean  "admin",                  default: false
+    t.boolean  "active",                 default: true
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
